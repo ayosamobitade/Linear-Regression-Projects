8652890523cd4aa11064922600f1ec102c0f3c7a
UncleAyo <ayosamobitade@gmail.com>
2022-05-25 12:39:16 +0100

    GPA predictor

--- Students GPA Predictor/GPA Predictions With R programing.R
+++ Students GPA Predictor/GPA Predictions With R programing.R
@@ -1,1 +1,18 @@
-# Prdicting Student GPA using in R
+# Predicting Student GPA using in R
+
+# Import the dataset
+
+dataset <- read.csv("1.02. Multiple linear regression.csv")
+head(dataset,4)
+
+# fiting the dataset on linear regression
+
+regressor = lm(GPA~.,
+               data = dataset)
+
+
+# making some predictions with the data
+y_pred = predict(regressor,
+                 newdata = dataset)
+
+y_pred
