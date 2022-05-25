76db81e61a07b80e916babf43b2502d16280e878
UncleAyo <ayosamobitade@gmail.com>
2022-05-25 11:36:31 +0100

    regression with R programing

---
+++ Startup Profit Predictions (With Python and R)/Startup Profit Prediction With R Programming.R
@@ -0,0 +1,29 @@
+# Startup Profit Predictions
+
+# Import the dataset
+
+dataset = read.csv('50_startups.csv')
+
+# Checking the output of the dataset
+head(dataset,4)
+
+# Encoding the "State" column categorical data
+str(dataset$State)
+
+dataset$State = factor(dataset$State, 
+                       levels = c('New York', 'California', 'Florida'), 
+                       labels = c(1,2,3))
+
+head(dataset, 4)
+
+# splitting the dataset into training and test set
+library(caTools)
+
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split==TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#
+
+
